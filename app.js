debc475ea6c24bbdae80d00f4398d5d9c8364e28
IgorValetskii <blrapok@gmail.com>
2019-10-15 14:24:56 +0300

    add authentication with JWT

--- app.js
+++ app.js
@@ -16,19 +16,47 @@ const options = {
         },
     },
     // Path to the API docs
-    apis: ['./routes/users.js','./routes/leagues.js','./routes/stages.js','./routes/races.js' ]
+    apis: ['./routes/users.js', './routes/leagues.js', './routes/stages.js', './routes/races.js']
 };
 
 // Initialize swagger-jsdoc -> returns validated swagger spec in json format
 const swaggerSpec = swaggerJSDoc(options);
 
 
+// app.get('/api-docs.json', (req, res) => {
+//     res.setHeader('Content-Type', 'application/json');
+//     res.send(swaggerSpec);
+// });
 
-app.get('/api-docs.json', (req, res) => {
-    res.setHeader('Content-Type', 'application/json');
-    res.send(swaggerSpec);
+app.post('/api/login', (req, res) => {
+    const admin = {
+        id: 1,
+        username: 'brad',
+        email: 'brad@gmail.com'
+    };
+    jwt.sign({admin}, 'secretkey', {expiresIn: '1d'}, (err, token) => {
+        res.json({token});
+    })
 });
 
+function verifyToken(req, res, next) {
+    //Get auth header value
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[1];
+        req.token = bearerToken;
+    } else {
+        res.sendStatus(403);
+    }
+    jwt.verify(req.token, 'secretkey', (err, authData) => {
+        if (err) {
+            res.sendStatus(403);
+
+        }
+        next();
+    });
+}
 
 //Routes
 const users = require('./routes/users');
@@ -38,15 +66,15 @@ const races = require('./routes/races');
 
 //Middlewares
 app.use(bodyParser.urlencoded({
-   extended: false
+    extended: false
 }));
 app.use(bodyParser.json());
 
 //Routes
-app.use('/users', users);
-app.use('/leagues',leagues);
-app.use('/stages',stages);
-app.use('/races',races);
+app.use('/users', verifyToken, users);
+app.use('/leagues', verifyToken, leagues);
+app.use('/stages', verifyToken, stages);
+app.use('/races', verifyToken, races);
 
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
@@ -59,10 +87,10 @@ async function start() {
             useUnifiedTopology: true
         });
 
-        app.listen(3000,  () => {
+        app.listen(3000, () => {
             console.log('Example app listening on port 3000!');
         });
-    }catch (e) {
+    } catch (e) {
         console.log(e);
 
     }
